90943d5e0ad91b153b9835829494eee14b78291c
suny <1078460760@qq.com>
2024-03-14 00:03:22 +0800

    add friendly-snippets plugins

--- init.lua
+++ init.lua
@@ -641,6 +641,33 @@ require('lazy').setup {
           end
           return 'make install_jsregexp'
         end)(),
+        dependencies = {
+          'rafamadriz/friendly-snippets',
+          'benfowler/telescope-luasnip.nvim',
+        },
+        --friendly-snippets的wiki的代码,用于代码模板的插件
+        config = function(_, opts)
+          if opts then
+            require('luasnip').config.setup(opts)
+          end
+          vim.tbl_map(function(type)
+            require('luasnip.loaders.from_' .. type).lazy_load()
+          end, { 'vscode', 'snipmate', 'lua' })
+          -- friendly-snippets - enable standardized comments snippets
+          require('luasnip').filetype_extend('typescript', { 'tsdoc' })
+          require('luasnip').filetype_extend('javascript', { 'jsdoc' })
+          require('luasnip').filetype_extend('lua', { 'luadoc' })
+          require('luasnip').filetype_extend('python', { 'pydoc' })
+          require('luasnip').filetype_extend('rust', { 'rustdoc' })
+          require('luasnip').filetype_extend('cs', { 'csharpdoc' })
+          require('luasnip').filetype_extend('java', { 'javadoc' })
+          require('luasnip').filetype_extend('c', { 'cdoc' })
+          require('luasnip').filetype_extend('cpp', { 'cppdoc' })
+          require('luasnip').filetype_extend('php', { 'phpdoc' })
+          require('luasnip').filetype_extend('kotlin', { 'kdoc' })
+          require('luasnip').filetype_extend('ruby', { 'rdoc' })
+          require('luasnip').filetype_extend('sh', { 'shelldoc' })
+        end,
       },
       'saadparwaiz1/cmp_luasnip',
 
@@ -661,6 +688,8 @@ require('lazy').setup {
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
+      luasnip.filetype_extend('c', { 'cdoc' })
+      luasnip.filetype_extend('cpp', { 'cppdoc' })
 
       -- autopairs支持
       local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
